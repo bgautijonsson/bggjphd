212a360d5d452823bef81c976d11a666882edc05
bgautijonsson <bgautijonsson@gmail.com>
2023-01-28 20:46:55 +0000

    Removing group_df from data-raw.

--- data-raw/group_df.R
+++ data-raw/group_df.R
@@ -1,26 +1,26 @@
-## code to prepare `group_df` dataset goes here
-
-group_df <- crossing(
-  x = 1:150,
-  y = 1:200
-) |>
-  mutate(
-    x_group = floor((x - 1) / 50) + 1,
-    y_group = floor((y - 1) / 50) + 1,
-    group = paste(x_group, y_group) |> as_factor() |> as.numeric()
-  ) |>
-  select(
-    proj_x = x,
-    proj_y = y,
-    group
-  ) |>
-  group_by(group) |>
-  reframe(
-    min_x = min(proj_x),
-    max_x = max(proj_x),
-    min_y = min(proj_y),
-    max_y = max(proj_y),
-    n_stations = n()
-  )
-
-usethis::use_data(group_df, overwrite = TRUE)
+# ## code to prepare `group_df` dataset goes here
+#
+# group_df <- crossing(
+#   x = 1:150,
+#   y = 1:200
+# ) |>
+#   mutate(
+#     x_group = floor((x - 1) / 50) + 1,
+#     y_group = floor((y - 1) / 50) + 1,
+#     group = paste(x_group, y_group) |> as_factor() |> as.numeric()
+#   ) |>
+#   select(
+#     proj_x = x,
+#     proj_y = y,
+#     group
+#   ) |>
+#   group_by(group) |>
+#   reframe(
+#     min_x = min(proj_x),
+#     max_x = max(proj_x),
+#     min_y = min(proj_y),
+#     max_y = max(proj_y),
+#     n_stations = n()
+#   )
+#
+# usethis::use_data(group_df, overwrite = TRUE)
---
+++ results/analogsea/analogsea.R
@@ -0,0 +1,61 @@
+library(analogsea)
+library(bggjphd)
+library(tidyverse)
+library(future)
+
+d <- docklet_create()
+
+d |> docklet_pull("r-base")
+
+d |> droplet_ssh("docker ps", verbose = TRUE)
+
+
+
+ip <- d |> droplet_ip()
+
+ssh_private_key_file <- "/Users/brynjolfurjonsson/.ssh/id_rsa"
+
+# Connect and create a cluster
+cl <- makeClusterPSOCK(
+  ip,
+
+  # User name; DigitalOcean droplets use root by default
+  user = "root",
+
+  # Use private SSH key registered with DigitalOcean
+  rshopts = c(
+    "-o", "StrictHostKeyChecking=no",
+    "-o", "IdentitiesOnly=yes",
+    "-i", ssh_private_key_file
+  ),
+
+  # Command to run on each remote machine
+  # The script loads the tidyverse Docker image
+  # --net=host allows it to communicate back to this computer
+  rscript = c("sudo", "docker", "run", "--net=host",
+              "rocker/tidyverse", "Rscript"),
+
+  # These are additional commands that are run on the remote machine.
+  # At minimum, the remote machine needs the future library to work—installing furrr also installs future.
+  rscript_args = c(
+    # Create directory for package installation
+    "-e", shQuote("local({p <- Sys.getenv('R_LIBS_USER'); dir.create(p, recursive = TRUE, showWarnings = FALSE); .libPaths(p)})"),
+    # Install furrr and future
+    "-e", shQuote("if (!requireNamespace('furrr', quietly = TRUE)) install.packages('furrr')"),
+    # Install pak for package management
+    "-e", shQuote("if (!requireNamespace('remotes', quietly = TRUE)) install.packages('remotes')"),
+    # Install PHD Package
+    "-e", shQuote("if (!requireNamespace('bggjphd', quietly = TRUE)) remotes::install_github('bgautijonsson/bggjphd')")
+  ),
+
+  # Actually run this stuff. Set to TRUE if you don't want it to run remotely.
+  dryrun = FALSE
+)
+
+
+plan(cluster, workers = cl)
+
+n_cpus %<-% {bggjphd::stations}
+n_cpus
+
+plan(sequential)
